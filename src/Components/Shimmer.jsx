33c17ce9aaf035cbd8030d6c9dc90571d36860e8
Kautilya-23 <kautilyasathwara@gmail.com>
2025-07-05 19:45:05 +0530

    some changes

---
+++ src/Components/Shimmer.jsx
@@ -0,0 +1,16 @@
+const Shimmer = () =>{
+    return(
+        <div className="shimmer flex-wrap flex">
+            <div className="shimmer-card p-2 m-2 w-72 h-72 bg-slate-100" />
+            <div className="shimmer-card p-2 m-2 w-72 h-72 bg-slate-100" />
+            <div className="shimmer-card p-2 m-2 w-72 h-72 bg-slate-100" />
+            <div className="shimmer-card p-2 m-2 w-72 h-72 bg-slate-100" />
+            <div className="shimmer-card p-2 m-2 w-72 h-72 bg-slate-100" />
+            <div className="shimmer-card p-2 m-2 w-72 h-72 bg-slate-100" />
+            <div className="shimmer-card p-2 m-2 w-72 h-72 bg-slate-100" />
+            <div className="shimmer-card p-2 m-2 w-72 h-72 bg-slate-100" />
+        </div>
+    )
+}
+
+export default Shimmer;
